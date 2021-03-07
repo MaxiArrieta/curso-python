825133036307a9ea113b0dcbe2d26b48638bbd8b
Maxi Arrieta <arrieta.maxi18@gmail.com>
2021-03-07 01:10:06 -0300

    ejercicio de bucles

---
+++ bucles.py
@@ -0,0 +1,13 @@
+def run():
+  print('Potencias de 2 hasta 1000')
+  LIMITE = 1000
+  i = 1
+  potencia_2 = 2 ** i
+  while potencia_2 < LIMITE:
+    print('2 ^ ' + str(i) + ' = ' + str(potencia_2) + '\n')
+    i += 1
+    potencia_2 = 2 ** i
+
+
+if __name__ == '__main__':
+  run()
